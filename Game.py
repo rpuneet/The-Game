76dc5cd86392e70fa276f2be67bf0b413b6b1798
Puneet Rai <puneetrai04@gmail.com>
2018-06-25 10:01:23 +0530

    Add pacman ghost collisions

--- Game.py
+++ Game.py
@@ -24,7 +24,8 @@ WINDOW_WIDTH = 900
 CAPTION = "PACMAN"
 LEVEL = 1
 GHOST_RELEASE_DELAY = 5000
-
+CELL_WIDTH = 24
+CELL_HEIGHT = 24
 
 # Utility Finctions
 
@@ -41,6 +42,43 @@ def check_win(maze):
     
     return True
 
+def release_ghost_in_maze(ghosts_in_maze , ghosts_not_in_maze):
+    '''
+    Releases ghosts in the maze after every 5 seconds.
+    It transfers ghosts not in maze to ghost in maze and changes its in_maze value to True.
+
+    Parameters-
+        ghost_in_maze - list of ghosts that are in maze.
+        ghost_not_in_maze - list of ghosts that are in ghost box.
+    '''
+    if pygame.time.get_ticks() >= (len(ghosts_in_maze) + 1) * GHOST_RELEASE_DELAY:
+        ghost_to_release = ghosts_not_in_maze.pop()
+        ghost_to_release.in_maze = True
+        ghosts_in_maze.append(ghost_to_release)
+
+def game_won():
+    pygame.time.delay(2000)
+
+def game_over():
+    pygame.time.delay(2000)
+
+def check_ghost_collision(pacman , ghost):
+    pacman_x_index = round(pacman.x / CELL_WIDTH)
+    pacman_y_index = round(pacman.y / CELL_HEIGHT)
+
+    ghost_x_index = round(ghost.x / CELL_WIDTH)
+    ghost_y_index = round(ghost.y / CELL_HEIGHT)
+
+    if(ghost_x_index == pacman_x_index and ghost_y_index == pacman_y_index):
+        return True
+
+def check_ghosts_collision(pacman , ghosts_in_maze):
+    for ghost in ghosts_in_maze:
+        if check_ghost_collision(pacman , ghost):
+            return True
+    return False
+    
+    
 
 
 # Initialising the screen.
@@ -96,16 +134,19 @@ while True:
     # Update the screen.
     pygame.display.update()
 
-    # Check if all the pellets are gone.
-    if check_win(maze):
-        pygame.time.delay(100)
+    # Check for collision with ghost.
+    if check_ghosts_collision(pacman , ghosts_in_maze):
+        game_over()
         exit(0)
 
-    # Release 1 ghost in the maze after every 5 second.
-    if pygame.time.get_ticks() >= (len(ghosts_in_maze) + 1) * GHOST_RELEASE_DELAY and len(ghosts_not_in_maze) > 0:
-        ghost_to_release = ghosts_not_in_maze.pop()
-        ghost_to_release.in_maze = True
-        ghosts_in_maze.append(ghost_to_release)
+    # Check if all the pellets are gone.
+    if check_win(maze):
+        game_won()
+        exit(0)
+
+    # Release 1 ghost in the maze after every 5 second. if there is a ghost to release.
+    if len(ghosts_not_in_maze) > 0:
+        release_ghost_in_maze(ghosts_in_maze , ghosts_not_in_maze)
 
 
     clock.tick(FPS)     # Maintains the fps at a particular value.
